938715084e1e9dbcd8d53a1faf55da6359cde08a
dio067 <yousefnas06@gmail.com>
2026-07-30 16:42:10 +0400

    feat(server): use port from app.config

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
-const PORT = process.env.PORT || 5000;
+import { port } from "./config";
 
 const app = express();
 
-app.listen(PORT, async () => {
-  console.log(`Server Running in PORT ${PORT} `);
+app.listen(port, async () => {
+  console.log(`Server Running in PORT ${port} `);
 });
