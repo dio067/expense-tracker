bb503efc8ae67f3715b930ec78c1f1616eb01fd2
dio067 <yousefnas06@gmail.com>
2026-07-31 10:54:17 +0400

    feat(config): add node_env and export it from app configs

--- packages/server/src/config/app.config.ts
+++ packages/server/src/config/app.config.ts
@@ -1,6 +1,7 @@
 const appConfig = {
   host: process.env.HOST,
   port: parseInt(process.env.PORT as string),
+  node_env: process.env.NODE_ENV,
 };
 
-export const { host, port } = appConfig;
+export const { host, port, node_env } = appConfig;
