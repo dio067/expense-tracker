65135e7b247de989235c40b6b945c0c2a2e6422a
dio067 <yousefnas06@gmail.com>
2026-08-10 17:39:03 +0400

    feat(auth-route): include all auth routes: login & register & refreshToken

--- packages/server/src/routes/auth.route.ts
+++ packages/server/src/routes/auth.route.ts
@@ -1,9 +1,12 @@
-import { Router, Request, Response } from "express";
-import { authenticateUser } from "../middlewares";
-import { logout } from "../controllers";
+import { Router } from "express";
+import { authenticateUser, refreshTokenValidation } from "../middlewares";
+import { login, logout, refreshToken, register } from "../controllers";
 
 const router = Router();
 
+router.post("/login", login);
+router.post("/register", register);
 router.post("/logout", authenticateUser, logout);
+router.post("/refresh-token", refreshTokenValidation, refreshToken);
 
 export default router;
