4b5e0af7be0565953a6899af432310ea54a82a81
dio067 <yousefnas06@gmail.com>
2026-08-14 11:01:50 +0400

    refactor(server): mount routers and middleware in entrypoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { port } from "./config";
-
+import appRoutes from "./routes";
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use("/api", appRoutes);
+
+app.get("/");
 app.listen(port, async () => {
   console.log(`Server Running in PORT ${port} `);
 });
