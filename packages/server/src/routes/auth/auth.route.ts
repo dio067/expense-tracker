9dce036752cdf86fdec26b686ef4323c6a657277
dio067 <yousefnas06@gmail.com>
2026-08-14 13:14:43 +0400

    feat(server): wire up auth routes

---
+++ packages/server/src/routes/auth/auth.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { authenticateUser, refreshTokenValidation } from "../../middlewares";
+import { login, logout, refreshToken, register } from "../../controllers";
+
+const router = Router();
+
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", authenticateUser, logout);
+router.post("/refresh-token", refreshTokenValidation, refreshToken);
+
+export default router;
