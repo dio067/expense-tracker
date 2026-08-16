02494833ddf6858f77af6bf82bd91dd188ec2109
dio067 <yousefnas06@gmail.com>
2026-08-16 16:01:50 +0400

    feat(web): add auth store

---
+++ packages/web/src/store/auth.store.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import type { User } from "@/types";
+
+type AuthState = {
+  user: User | null;
+  isAuthenticated: boolean;
+  error: string | null;
+  isLoading: boolean;
+  setUser: (user: User | null) => void;
+  setIsLoading: (isLoading: boolean) => void;
+  setError: (error: string | null) => void;
+  signout: () => void;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  setUser: (user: User | null) => set({ user, isAuthenticated: !!user }),
+  setIsLoading: (isLoading: boolean) => set({ isLoading }),
+  setError: (error: string | null) => set({ error }),
+  signout: () =>
+    set({
+      user: null,
+      isAuthenticated: false,
+      isLoading: false,
+      error: null,
+    }),
+}));
