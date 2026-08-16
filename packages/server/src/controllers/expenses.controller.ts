b91584135bce934bbb9d41fd7d470db81addc924
dio067 <yousefnas06@gmail.com>
2026-08-16 12:47:41 +0400

    feat(server): implement expenses CRUD controller

--- packages/server/src/controllers/expenses.controller.ts
+++ packages/server/src/controllers/expenses.controller.ts
@@ -66,3 +66,78 @@ const expenseController = {
           category,
         },
       });
+
+      return res.status(201).json({
+        ok: true,
+        message: "Expense created successfully",
+        data: expense,
+      });
+    } catch (err) {
+      res.status(500).json({
+        ok: false,
+        message: "Failed to create data ",
+        data: null,
+      });
+    }
+  },
+
+  deleteExpense: async (req: Request, res: Response) => {
+    const { id } = (req as any).params;
+    const userId = (req as any).userId;
+
+    try {
+      const expense = await prisma.expenses.delete({
+        where: { id: parseInt(id), userId },
+      });
+
+      return res.status(200).json({
+        ok: true,
+        message: "Data deleted successfully",
+        data: null,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        ok: false,
+        message: "Data can't be deleted",
+        data: null,
+      });
+    }
+  },
+
+  updateExpense: async (req: Request, res: Response) => {
+    const { id } = (req as any).params;
+    const { description, amount, category } = req.body;
+    const userId = (req as any).userId;
+
+    try {
+      const expense = await prisma.expenses.update({
+        where: { id: parseInt(id), userId },
+        data: {
+          description,
+          amount,
+          category,
+        },
+      });
+
+      return res.status(200).json({
+        ok: true,
+        message: "Data altered successfully",
+        data: expense,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        ok: false,
+        message: "Data can't be altered",
+        data: null,
+      });
+    }
+  },
+};
+
+export const {
+  getExpense,
+  getExpenses,
+  addExpense,
+  deleteExpense,
+  updateExpense,
+} = expenseController;
