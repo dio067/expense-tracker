d255ed00577282b38b5a526275ce11e70a4c1c2c
dio067 <yousefnas06@gmail.com>
2026-08-16 21:15:21 +0400

    feat(server): add auth middleware to protect routes

--- packages/server/src/middlewares/auth.middlewares.ts
+++ packages/server/src/middlewares/auth.middlewares.ts
@@ -3,7 +3,7 @@ import * as jwt from "jsonwebtoken";
 import { refresh, secret } from "../config";
 
 interface decodedToken {
-  userId: string;
+  userId: number;
 }
 
 const authMiddlewares = {
@@ -11,7 +11,7 @@ const authMiddlewares = {
     const token = req.cookies.accessToken;
 
     if (!token) {
-      res
+      return res
         .status(401)
         .json({ ok: false, message: "No access token provided", data: null });
     }
@@ -30,37 +30,3 @@ const authMiddlewares = {
       });
     }
   },
-
-  refreshTokenValidation: async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-  ) => {
-    const refreshToken = req.cookies.refreshToken;
-
-    if (!refreshToken) {
-      res
-        .status(401)
-        .json({ ok: false, message: "No refresh token provided", data: null });
-    }
-    try {
-      const decodedToken = jwt.verify(refreshToken, refresh) as {
-        userId: number;
-      };
-
-      (req as any).userId = decodedToken.userId;
-
-      next();
-    } catch (err) {
-      console.error("Refresh Token authentication failed:", err);
-
-      return res.status(401).json({
-        ok: false,
-        message: "Invalid or expired refresh token",
-        data: null,
-      });
-    }
-  },
-};
-
-export const { authenticateUser, refreshTokenValidation } = authMiddlewares;
