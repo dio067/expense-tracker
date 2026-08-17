bce9bcef034d6029d4141de3a928fff66e1f52d0
dio067 <yousefnas06@gmail.com>
2026-08-17 16:55:34 +0400

    feat(web): add auth service

--- packages/web/src/services/auth.service.ts
+++ packages/web/src/services/auth.service.ts
@@ -11,3 +11,37 @@ type AuthorizedResponse = {
   message: string;
   data: User | null;
 };
+
+const AuthService = {
+  authMe: async (): Promise<AuthorizedResponse> => {
+    const res = await api.get("/auth/me");
+    return res.data;
+  },
+  update: async (payload: AlterUserPayload) => {
+    const res = await api.put("/auth/update", payload);
+    return res.data;
+  },
+  register: async (payload: {
+    name: string;
+    email: string;
+    password: string;
+  }) => {
+    const res = await api.post<AuthResponse>("/auth/register", payload);
+    return res.data;
+  },
+  login: async (payload: { email: string; password: string }) => {
+    const res = await api.post("/auth/login", payload);
+    return res.data;
+  },
+  logout: async () => {
+    try {
+      await api.post("/auth/logout");
+    } catch (err) {
+      console.error("Failed to logout", err);
+    } finally {
+      window.location.href = "/login";
+    }
+  },
+};
+
+export const { authMe, login, update, register, logout } = AuthService;
