7d3e84d7e299dc6db93618728ec48c87a2732ed1
dio067 <yousefnas06@gmail.com>
2026-08-19 00:16:55 +0400

    feat(web): add expense store

--- packages/web/src/store/expense.store.ts
+++ packages/web/src/store/expense.store.ts
@@ -1,2 +1,41 @@
 import { create } from "zustand";
 import type { Expense } from "@/types";
+
+type ExpensesState = {
+  expenses: Expense[];
+  selectedExpense: Expense | null;
+  isLoading: boolean;
+  error: string | null;
+  setIsLoading: (isLoading: boolean) => void;
+  setError: (error: string | null) => void;
+  setSelectedExpense: (selectedExpense: Expense) => void;
+  setExpenses: (expenses: Expense[]) => void;
+  addExpense: (expense: Expense) => void;
+  editExpense: (id: number, updated: Partial<Expense>) => void;
+  removeExpense: (id: number) => void;
+};
+export const useExpenseStore = create<ExpensesState>((set) => ({
+  expenses: [],
+  selectedExpense: null,
+  isLoading: false,
+  error: null,
+  setIsLoading: (isLoading) => set({ isLoading }),
+  setError: (error) => set({ error }),
+  setSelectedExpense: (selectedExpense) => set({ selectedExpense }),
+  setExpenses: (expenses) => set({ expenses }),
+  addExpense: (expense) =>
+    set((state) => ({
+      expenses: [...state.expenses, expense],
+    })),
+  editExpense: (id, updated) =>
+    set((state) => ({
+      expenses: state.expenses.map((exp) =>
+        exp.id === id ? { ...exp, ...updated } : exp,
+      ),
+    })),
+  removeExpense: (id) => {
+    set((state) => ({
+      expenses: state.expenses.filter((exp) => exp.id !== id),
+    }));
+  },
+}));
