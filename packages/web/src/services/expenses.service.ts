25100035b1e35373ac8179f36f19433d37976f5d
dio067 <yousefnas06@gmail.com>
2026-08-19 16:07:30 +0400

    feat(web): add expenses service

--- packages/web/src/services/expenses.service.ts
+++ packages/web/src/services/expenses.service.ts
@@ -10,3 +10,30 @@ interface ApiResponse<T> {
   message: string;
   data: T;
 }
+
+export const expenseService = {
+  create: async (
+    payload: createExpensePayload,
+  ): Promise<ApiResponse<Expense>> => {
+    const res = await api.post("/expenses", payload);
+    return res.data;
+  },
+  read: async (id: number): Promise<ApiResponse<Expense>> => {
+    const res = await api.get(`/expenses/${id}`);
+    return res.data;
+  },
+  readAll: async (): Promise<ApiResponse<Expense[]>> => {
+    const res = await api.get("/expenses");
+    return res.data;
+  },
+  update: async (
+    id: number,
+    payload: updateExpensePayload,
+  ): Promise<ApiResponse<Expense>> => {
+    const res = await api.put(`/expenses/${id}`, payload);
+    return res.data;
+  },
+  delete: async (id: number): Promise<void> => {
+    await api.delete(`/expenses/${id}`);
+  },
+};
