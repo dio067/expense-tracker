681c4f5c21fd9ffa43105d1027009cdd3dfe2e7b
dio067 <yousefnas06@gmail.com>
2026-08-19 14:43:01 +0400

    feat(web): add useAuth hook

---
+++ packages/web/src/hooks/useAuth.ts
@@ -0,0 +1,59 @@
+import { authMe, update, login, register, logout } from "@/services";
+import { useAuthStore } from "@/store/auth.store";
+import type { AlterUserPayload } from "@/types";
+
+export const useAuth = () => {
+  const { user, setUser, setIsLoading, setError, signout, isAuthenticated } =
+    useAuthStore();
+  const checkAuth = async () => {
+    setIsLoading(true);
+    try {
+      const res = await authMe();
+      if (res.ok && res.data) {
+        setUser(res.data);
+        return isAuthenticated;
+      } else {
+        setUser(null);
+      }
+    } catch (error) {
+      setUser(null);
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const patchUser = async (payload: AlterUserPayload) => {
+    setIsLoading(true);
+    try {
+      const res = await update(payload);
+      setUser(res.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const handleLogin = async (payload: { email: string; password: string }) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const res = await login(payload);
+      setUser({ id: res.id, name: res.name, email: res.email });
+    } catch (err: unknown) {
+      const apiError = err as {
+        response?: { data?: { error?: string } };
+        message?: string;
+      };
+      const msg = apiError.response?.data?.error ?? "Login failed";
+      console.log(
+        "ERROR:",
+        JSON.stringify(apiError.response?.data),
+        apiError.message,
+      );
+      setError(msg);
+      throw err;
+    } finally {
+      setIsLoading(false);
+    }
+  };
