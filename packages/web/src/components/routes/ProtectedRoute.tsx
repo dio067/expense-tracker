c9a009a7763ebe24ab25be98a742a825c6c110e7
dio067 <yousefnas06@gmail.com>
2026-08-20 15:05:24 +0400

    feat(web): add protected route wrapper

---
+++ packages/web/src/components/routes/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { useAuthStore } from "@/store/auth.store";
+import { useNavigate } from "react-router-dom";
+
+export function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { checkAuth } = useAuth();
+  const { isAuthenticated, isLoading } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isLoading, isAuthenticated]);
+
+  return isAuthenticated ? children : null;
+}
