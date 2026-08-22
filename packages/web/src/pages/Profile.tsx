9ed59ba86fabb45ad787675bffea47175e0e027b
dio067 <yousefnas06@gmail.com>
2026-08-22 19:39:53 +0400

    feat(web): add profile page

---
+++ packages/web/src/pages/Profile.tsx
@@ -0,0 +1,34 @@
+import { MoltenMetal } from "@/components";
+import { ProfileForm } from "@/components/forms/ProfileForm";
+import { useAuth } from "@/hooks/useAuth";
+
+export function Profile() {
+  const { user } = useAuth();
+  return (
+    <div className='relative min-h-screen p-10 bg-black'>
+      <div className='absolute inset-0 z-0'>
+        <MoltenMetal
+          color1='#0B0D17'
+          color2='#151A2E'
+          color3='#FFFFFF'
+          speed={0.8}
+          scale={4}
+          detail={3}
+          glow={1.6}
+          coreSize={0.1}
+          swirl={1}
+          fold={-0.2}
+          blackPoint={0.05}
+          brightness={1.3}
+          colorMode='molten'
+          grain
+          grainIntensity={0.05}
+          mouseInteraction
+          mouseStrength={0.3}
+          opacity={1}
+        />
+      </div>
+      <ProfileForm user={user} />
+    </div>
+  );
+}
