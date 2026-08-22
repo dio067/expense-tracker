08116f1a175e32d5b6b6f0fbe8be5519dd2a5bb1
dio067 <yousefnas06@gmail.com>
2026-08-22 18:26:51 +0400

    feat(web): add login page

---
+++ packages/web/src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import { LoginForm, Ferrofluid } from "@/components";
+
+export function Login() {
+  return (
+    <main className='relative md:h-full min-h-screen bg-black px-4 md:px-8 items-center justify-center flex flex-col'>
+      <div className='absolute inset-0 z-0'>
+        <Ferrofluid
+          colors={["#ffffff", "#ffffff", "#ffffff"]}
+          speed={0.5}
+          scale={1.6}
+          turbulence={1}
+          fluidity={0.1}
+          rimWidth={0.2}
+          sharpness={2.5}
+          shimmer={1.5}
+          glow={2}
+          flowDirection='down'
+          opacity={1}
+          mouseInteraction
+          mouseStrength={1}
+          mouseRadius={0.35}
+        />
+      </div>
+      <LoginForm />
+    </main>
+  );
+}
