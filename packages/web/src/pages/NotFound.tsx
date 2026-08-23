4b7bb847e8683e5fb8617b6b903ef9d58cbaafbe
dio067 <yousefnas06@gmail.com>
2026-08-23 21:36:40 +0400

    feat(web): add 404 page

---
+++ packages/web/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import { FaceSlightlyFrowning } from "lucide-react";
+
+export function NotFound() {
+  return (
+    <main className='flex flex-col items-center justify-center relative md:h-full min-h-screen bg-black px-4 md:px-8 '>
+      <FaceSlightlyFrowning className='h-40 w-40 text-gray-400' />
+      <h1 className='m-4 text-4xl text-gray-400'>Page Not Found</h1>
+    </main>
+  );
+}
