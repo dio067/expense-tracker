d5093c6d8e362b009c3ac8768f895efdc84d6611
dio067 <yousefnas06@gmail.com>
2026-08-29 15:13:20 +0400

    feat(server): add the validate middleware to all put and put endpoints

--- packages/server/src/routes/auth/auth.route.ts
+++ packages/server/src/routes/auth/auth.route.ts
@@ -3,13 +3,15 @@ import {
   authenticateUser,
   authLimiter,
   refreshTokenValidation,
+  validate,
 } from "../../middlewares";
 import { login, logout, refreshToken, register } from "../../controllers";
+import { loginSchema, registerSchema } from "@/schemas/auth.schema";
 
 const router = Router();
 
-router.post("/login", authLimiter, login);
-router.post("/register", authLimiter, register);
+router.post("/login", authLimiter, validate(loginSchema), login);
+router.post("/register", authLimiter, validate(registerSchema), register);
 router.post("/logout", authenticateUser, logout);
 router.post("/refresh-token", refreshTokenValidation, refreshToken);
 
--- packages/server/src/routes/expenses/expenses.route.ts
+++ packages/server/src/routes/expenses/expenses.route.ts
@@ -5,8 +5,13 @@ import {
   getExpenses,
   updateExpense,
 } from "../../controllers";
+import { validate } from "../../middlewares";
 import { authenticateUser } from "../../middlewares";
 import { Router } from "express";
+import {
+  createExpenseSchema,
+  updateExpenseSchema,
+} from "@/schemas/expenses.schema";
 
 const router = Router();
 
@@ -14,8 +19,8 @@ router.use(authenticateUser);
 
 router.get("/", getExpenses);
 router.get("/:id/", getExpense);
-router.post("/", addExpense);
+router.post("/", validate(createExpenseSchema), addExpense);
 router.delete("/:id/", deleteExpense);
-router.put("/:id/", updateExpense);
+router.put("/:id/", validate(updateExpenseSchema), updateExpense);
 
 export default router;
